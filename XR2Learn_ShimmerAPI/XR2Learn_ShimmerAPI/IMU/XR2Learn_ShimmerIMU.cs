64bfbd262fd5ff3d6fd5e64e790fc7211a789b58
tatiana.dalbusco <tatiana.dalbusco@student.supsi.ch>
2025-08-01 11:02:25 +0200

    Add comments in XR2Learn_ShimmerIMU.cs

--- XR2Learn_ShimmerAPI/XR2Learn_ShimmerAPI/IMU/XR2Learn_ShimmerIMU.cs
+++ XR2Learn_ShimmerAPI/XR2Learn_ShimmerAPI/IMU/XR2Learn_ShimmerIMU.cs
@@ -9,28 +9,30 @@ namespace XR2Learn_ShimmerAPI.IMU
     /// <summary>
     /// Partial class that handles Shimmer IMU configuration, data acquisition,
     /// and real-time data parsing from a serial port connection.
+    /// Uses ShimmerLogAndStreamSystemSerialPortV2 for enhanced serial communication.
     /// </summary>
     public partial class XR2Learn_ShimmerIMU
     {
 
-        // Instance of the Shimmer serial port communication interface.
-        private ShimmerLogAndStreamSystemSerialPort shimmer;
+        // Instance of the enhanced Shimmer serial port communication class.
+        private ShimmerLogAndStreamSystemSerialPortV2 shimmer;
 
         private bool firstDataPacket = true;
 
+        // Indexes for sensor signal mapping
         private int indexTimeStamp;
-        private int indexAccX;
-        private int indexAccY;
-        private int indexAccZ;
+        private int indexLowNoiseAccX;
+        private int indexLowNoiseAccY;
+        private int indexLowNoiseAccZ;
+        private int indexWideAccX;
+        private int indexWideAccY;
+        private int indexWideAccZ;
         private int indexGyroX;
         private int indexGyroY;
         private int indexGyroZ;
         private int indexMagX;
         private int indexMagY;
         private int indexMagZ;
-        private int indexWideAccX;
-        private int indexWideAccY;
-        private int indexWideAccZ;
         private int indexBMP180Temperature;
         private int indexBMP180Pressure;
         private int indexBatteryVoltage;
@@ -38,17 +40,21 @@ namespace XR2Learn_ShimmerAPI.IMU
         private int indexExtA7;
         private int indexExtA15;
 
-
+        /// <summary>
+        /// Latest IMU data sample received from the Shimmer device.
+        /// </summary>
         public XR2Learn_ShimmerIMUData LatestData { get; private set; }
 
+        /// <summary>
+        /// Initializes default sensor settings and sampling rate.
+        /// </summary>
         public XR2Learn_ShimmerIMU()
         {
-            // Sampling rate and default sensor config
             _samplingRate = 51.2;
-            _enableGyroscope = true;
-            _enableMagnetometer = true;
             _enableLowNoiseAccelerometer = true;
             _enableWideRangeAccelerometer = true;
+            _enableGyroscope = true;
+            _enableMagnetometer = true;
             _enablePressureTemperature = true;
             _enableBattery = true;
             _enableExtA6 = true;
@@ -56,14 +62,31 @@ namespace XR2Learn_ShimmerAPI.IMU
             _enableExtA15 = true;
         }
 
-        public void Configure(string deviceName, string comPort,
-            bool enableLowNoiseAcc, bool enableWideRangeAcc,
-            bool enableGyro, bool enableMag, bool enablePressureTemp,
-            bool enableBattery, bool enableExtA6, bool enableExtA7, bool enableExtA15)
 
+        /// <summary>
+        /// Configures the Shimmer device connection and enabled sensors.
+        /// </summary>
+        /// <param name="deviceName">Device identifier string.</param>
+        /// <param name="comPort">Serial COM port name.</param>
+        /// <param name="enableLowNoiseAcc">Enable low-noise accelerometer.</param>
+        /// <param name="enableWideRangeAcc">Enable wide-range accelerometer.</param>
+        /// <param name="enableGyro">Enable gyroscope.</param>
+        /// <param name="enableMag">Enable magnetometer.</param>
+        /// <param name="enablePressureTemp">Enable BMP180 temperature/pressure.</param>
+        /// <param name="enableBattery">Enable battery voltage monitoring.</param>
+        /// <param name="enableExtA6">Enable external ADC A6.</param>
+        /// <param name="enableExtA7">Enable external ADC A7.</param>
+        /// <param name="enableExtA15">Enable external ADC A15.</param>
+        public void Configure(
+            string deviceName, string comPort, bool enableLowNoiseAcc, 
+            bool enableWideRangeAcc, bool enableGyro, bool enableMag, 
+            bool enablePressureTemp, bool enableBattery, 
+            bool enableExtA6, bool enableExtA7, bool enableExtA15
+        )
         {
             int enabledSensors = 0;
 
+            // Set sensor enable flags
             _enableLowNoiseAccelerometer = enableLowNoiseAcc;
             _enableWideRangeAccelerometer = enableWideRangeAcc;
             _enableGyroscope = enableGyro;
@@ -74,86 +97,70 @@ namespace XR2Learn_ShimmerAPI.IMU
             _enableExtA7 = enableExtA7;
             _enableExtA15 = enableExtA15;
 
-
+            // Build sensor bitmap
             if (_enableLowNoiseAccelerometer)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_A_ACCEL;
-            }
 
             if (_enableWideRangeAccelerometer)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_D_ACCEL;
-            }
 
             if (_enableGyroscope)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_MPU9150_GYRO;
-            }
 
             if (_enableMagnetometer)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_LSM303DLHC_MAG;
-            }
 
             if (_enablePressureTemperature)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_BMP180_PRESSURE;
-            }
 
             if (_enableBattery)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_VBATT;
-            }
 
             if (_enableExtA6)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_EXT_A6;
-            }
 
             if (_enableExtA7)
-            {
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_EXT_A7;
-            }
 
             if (_enableExtA15) 
-            { 
                 enabledSensors |= (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_EXT_A15;
-            }
 
+            // Short delay before initializing communication
             Thread.Sleep(500);
 
+            // Initialize the enhanced shimmer communication
+            shimmer = new ShimmerLogAndStreamSystemSerialPortV2(deviceName, comPort);
 
-
-
-
-            shimmer = new ShimmerLogAndStreamSystemSerialPort(
-                deviceName, comPort, _samplingRate,
-                0, 0, enabledSensors,
-                false, false, false,
-                0, 0,
-                Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP1,
-                Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP2,
-                true
-            );
-
-
+            // Register the data callback handler
             shimmer.UICallback += this.HandleEvent;
         }
 
+        /// <summary>
+        /// Handles incoming data packets from the Shimmer device and extracts sensor values.
+        /// </summary>
         private void HandleEvent(object sender, EventArgs args)
         {
             CustomEventArgs eventArgs = (CustomEventArgs)args;
+
+            // Verify that the received message is a data packet
             if (eventArgs.getIndicator() == (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_DATA_PACKET)
             {
+
+                // Gets the raw data collected from the device into an object called ObjectCluster
                 ObjectCluster oc = (ObjectCluster)eventArgs.getObject();
 
+                // When receiving for the first time, save the signal indexes for quick access later
                 if (firstDataPacket)
                 {
                     indexTimeStamp = oc.GetIndex(ShimmerConfiguration.SignalNames.SYSTEM_TIMESTAMP, ShimmerConfiguration.SignalFormats.CAL);
 
-                    indexAccX = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_X, ShimmerConfiguration.SignalFormats.CAL);
-                    indexAccY = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Y, ShimmerConfiguration.SignalFormats.CAL);
-                    indexAccZ = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Z, ShimmerConfiguration.SignalFormats.CAL);
+                    indexLowNoiseAccX = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_X, ShimmerConfiguration.SignalFormats.CAL);
+                    indexLowNoiseAccY = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Y, ShimmerConfiguration.SignalFormats.CAL);
+                    indexLowNoiseAccZ = oc.GetIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Z, ShimmerConfiguration.SignalFormats.CAL);
+
+                    indexWideAccX = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_X, ShimmerConfiguration.SignalFormats.CAL);
+                    indexWideAccY = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Y, ShimmerConfiguration.SignalFormats.CAL);
+                    indexWideAccZ = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Z, ShimmerConfiguration.SignalFormats.CAL);
 
                     indexGyroX = oc.GetIndex(Shimmer3Configuration.SignalNames.GYROSCOPE_X, ShimmerConfiguration.SignalFormats.CAL);
                     indexGyroY = oc.GetIndex(Shimmer3Configuration.SignalNames.GYROSCOPE_Y, ShimmerConfiguration.SignalFormats.CAL);
@@ -162,10 +169,6 @@ namespace XR2Learn_ShimmerAPI.IMU
                     indexMagX = oc.GetIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_X, ShimmerConfiguration.SignalFormats.CAL);
                     indexMagY = oc.GetIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_Y, ShimmerConfiguration.SignalFormats.CAL);
                     indexMagZ = oc.GetIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_Z, ShimmerConfiguration.SignalFormats.CAL);
-
-                    indexWideAccX = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_X, ShimmerConfiguration.SignalFormats.CAL);
-                    indexWideAccY = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Y, ShimmerConfiguration.SignalFormats.CAL);
-                    indexWideAccZ = oc.GetIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Z, ShimmerConfiguration.SignalFormats.CAL);
 
                     indexBMP180Temperature = oc.GetIndex(Shimmer3Configuration.SignalNames.TEMPERATURE, ShimmerConfiguration.SignalFormats.CAL);
                     indexBMP180Pressure = oc.GetIndex(Shimmer3Configuration.SignalNames.PRESSURE, ShimmerConfiguration.SignalFormats.CAL);
@@ -176,20 +179,22 @@ namespace XR2Learn_ShimmerAPI.IMU
                     indexExtA7 = oc.GetIndex(Shimmer3Configuration.SignalNames.EXTERNAL_ADC_A7, ShimmerConfiguration.SignalFormats.CAL);
                     indexExtA15 = oc.GetIndex(Shimmer3Configuration.SignalNames.EXTERNAL_ADC_A15, ShimmerConfiguration.SignalFormats.CAL);
 
-
                     firstDataPacket = false;
                 }
 
+                // Creates a new XR2Learn_ShimmerIMUData object containing all the sensor data and saves it in LatestData
                 LatestData = new XR2Learn_ShimmerIMUData(
+
+                    // Use the saved indices to extract the values of each sensor from the received packet
                     oc.GetData(indexTimeStamp),
 
-                    oc.GetData(indexAccX), oc.GetData(indexAccY), oc.GetData(indexAccZ),
+                    oc.GetData(indexLowNoiseAccX), oc.GetData(indexLowNoiseAccY), oc.GetData(indexLowNoiseAccZ),
+
+                    oc.GetData(indexWideAccX), oc.GetData(indexWideAccY), oc.GetData(indexWideAccZ),
 
                     oc.GetData(indexGyroX), oc.GetData(indexGyroY), oc.GetData(indexGyroZ),
 
                     oc.GetData(indexMagX), oc.GetData(indexMagY), oc.GetData(indexMagZ),
-
-                    oc.GetData(indexWideAccX), oc.GetData(indexWideAccY), oc.GetData(indexWideAccZ),
 
                     oc.GetData(indexBMP180Temperature), oc.GetData(indexBMP180Pressure),
                     
@@ -199,6 +204,5 @@ namespace XR2Learn_ShimmerAPI.IMU
                 );
             }
         }
-
     }
 }
